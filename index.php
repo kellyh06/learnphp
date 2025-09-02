9277cf12c2138f0fe2c9686b2540503e76b07370
Kelly Hurt <kelly.hurt@tptlive.ee>
2025-09-02 11:13:58 +0300

    Add types examples

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+
+echo "Hello World!\n";
+echo ("Hello World!\n");
+
+$var = 1;
+echo $var;
+echo "Var is $var";
+echo "\nVar is $var\n";
+echo $var . "\n";
+$num = 1;
+$num = $num + 1;
+$num ++;
+$num --;
+$num +=1;
+$num -=1;
+$num *=2;
+$num /=2;
+$num = 13;
+$num %=2;
+$num = 1.3;
+$num = 3;
+$num **=2;
+$num = true;
+$num = false;
+$bar;
+$bar = null;
+
+var_dump($num);

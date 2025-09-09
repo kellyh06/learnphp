77198caf7383f4df02f2749f7f38d7188181bfdb
kellyh06 <kelly.hurt@tptlive.ee>
2025-09-09 11:39:56 +0300

    objects by reference example

--- index.php
+++ index.php
@@ -16,20 +16,8 @@ class Box {
     }
 }
 
-$box1 = new Box(10, 10, 10);
-
+$box1 = new Box();
 $box1->width = 10;
-$box1->height = 10;
-$box1->length = 10;
-var_dump($box1);
-var_dump($box1->volume());
-
-
-$box2 = new Box(20, 20, 20);
-
+$box2 = clone $box1;
 $box2->width = 20;
-$box2->height = 20;
-$box2->length = 20;
-var_dump($box2);
-var_dump($box2->volume());
-var_dump($box1);
+var_dump($box1->width);

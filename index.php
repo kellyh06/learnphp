06de40c21c3c557d20e40a007a19bdd4947a8bd5
kellyh06 <kelly.hurt@tptlive.ee>
2025-09-09 10:49:47 +0300

    Add Loop examples

--- index.php
+++ index.php
@@ -1,40 +1,51 @@
 <?php
 
-$num = 15;
-if ($num > 10) {
-    var_dump('Suurem');
-} else if ($num < 10){
-    
-} else {
-    
+for ($i = 0; $i < 10; $i++) {
+    var_dump($i);
 }
 
-$day = date('w', strtotime('09.09.2025'));
-var_dump($day);
-
-switch ($day) {
-    case 1:
-        var_dump('Esmaspäev');
-        break;
-    case 2:
-        var_dump('Teisipäev');
-        break;
-    case 3:
-        var_dump('Kolmapäev');
-        break;
-    case 4:
-        var_dump('Neljapäev');
-        break;
-    case 5:
-        var_dump('Reede');
-        break;
-    case 6:
-        var_dump('Laupäev');
-        break;
-    case 0:
-        var_dump('Pühapäev');
-        break;
-    default:
-        var_dump('Tundmatu päev');
-        break;
+for ($i = 10; $i > 0; $i--) {
+    var_dump($i);
 }
+
+for ($i = 1; $i < 1_000_000; $i*=2) {
+    var_dump($i);
+}
+$time = time();
+$i = 0;
+while (time() < $time + 1) {
+    var_dump(time());
+}
+var_dump($i);
+
+$i = 10;
+while ($i > 10) {
+    var_dump('While');
+}
+do {
+    var_dump('DO');
+} while ($i > 10);
+
+$array = [1,2,3];
+
+foreach($array as $key => $value){
+    var_dump($key, $value);
+}
+
+foreach(array_keys($array) as $value) {
+    var_dump($value);
+}
+
+$a = 1;
+$b = &$a;
+$b = 2;
+var_dump($a);
+
+$array = [1,2,3];
+
+
+
+foreach($array as &$value) {
+    $value *= 2;
+}
+var_dump($array);

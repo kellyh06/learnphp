7d37ad9ecfa905f707bc6829edfe5c42f8b9efa9
kellyh06 <kelly.hurt@tptlive.ee>
2025-09-09 11:24:46 +0300

    Additional loop stuff

--- index.php
+++ index.php
@@ -1,52 +1,19 @@
 <?php
  
- 
-function hello() {
-    var_dumb( "Hello, World!");
-}
- 
-hello();
- 
-function helloName($name='Unknown'){
-    var_dump("Hello" . $name);
-}
-helloName("Kelly");
-helloName("Nikita");
- 
-function helloNameAndAge($name='Unknown', $age){
-    var_dump("Hello . $name and you are $age years old.");
-}
- 
-helloNameAndAge("Nikita", 20);
- 
-function stuff(...$args){
-    var_dump($args);
-}
- 
-stuff(1,2,3,4,5);
-
-function sum($a, $b){
-    return $a + $b;
-    var_dump('asdasdasd')
-}
-
-$answer = sum(1,5);
-var_dump($answer);
-
-function biggerOrSmaller($a){
-    if($a > 10){
-        return "Bigger";
-    } else {
-        return "Smaller";
+ for ($i = 0; $i < 10; $i++) {
+    if ($i == 3) {
+        continue;
     }
-}
-var_dump(biggerOrSmaller(5));
-var_dump(biggerOrSmaller(15));
-
-function recursive($i){
-    if($i < 10){
-        var_dump($i);
-        recursive($i + 1);
+    if( $i == 5) {
+        break;
     }
-}
-recursive(0);
+    var_dump($i);
+ }
+
+ for($i = 0; $i < 10; $i++) {
+    if ($j = 0; $j < 10; $j++) {
+        break 2;
+        var_dump($j);
+    }
+    var_dump($i);
+ }

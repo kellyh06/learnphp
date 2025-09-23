7f3091990c509a4d3875e9e3506bf11f7fe4817e
kellyh06 <kelly.hurt@tptlive.ee>
2025-09-23 10:31:31 +0300

    Move posts to router

--- public/index.php
+++ public/index.php
@@ -1,6 +1,6 @@
 <?php
 if (preg_match('/\.(?:png|jpg|jpeg|gif|css|js)$/', $_SERVER["REQUEST_URI"])) {
-    return false;    // serve the requested resource as-is.
+  return false;    // serve the requested resource as-is.
 }
 
 
@@ -8,13 +8,43 @@ if (preg_match('/\.(?:png|jpg|jpeg|gif|css|js)$/', $_SERVER["REQUEST_URI"])) {
 //var_dump($_SERVER);
 //echo '</pre>';
 
-switch($_SERVER['REQUEST_URI']) {
+switch ($_SERVER['REQUEST_URI']) {
   case '/':
+    $posts = [
+      [
+        'title' => 'US news 1',
+        'published' => '16.09.2025',
+        'author' => 'Nikita',
+        'body' => 'This is the biggest hit.'
+      ],
+
+      [
+        'title' => 'US news 2',
+        'published' => '15.09.2025',
+        'author' => 'Aleks',
+        'body' => 'This is always the best trick.'
+      ],
+
+      [
+        'title' => 'US news 3',
+        'published' => '14.09.2025',
+        'author' => 'Lylia',
+        'body' => 'This might be illegal.'
+      ],
+
+      [
+        'title' => 'US news 4',
+        'published' => '13.09.2025',
+        'author' => 'Morgan',
+        'body' => 'Say no to this.'
+      ],
+
+    ];
     include __DIR__ . '/../views/index.php';
     break;
-    case '/us':
-        include __DIR__ . '/../views/us.php';
-        break;
-    default:
-        echo '404 Page Not Found';
+  case '/us':
+    include __DIR__ . '/../views/us.php';
+    break;
+  default:
+    echo '404 Page Not Found';
 }
--- views/us.php
+++ views/us.php
@@ -1,41 +1,15 @@
-<?php
-$posts = [
-  ['title' => 'US news 1',
-   'published' => '16.09.2025',
-   'author' => 'Nikita',
-   'body' => 'This is the biggest hit.'],
-
-   ['title' => 'US news 2',
-   'published' => '15.09.2025',
-   'author' => 'Aleks',
-   'body' => 'This is always the best trick.'],
-
-   ['title' => 'US news 3',
-   'published' => '14.09.2025',
-   'author' => 'Lylia',
-   'body' => 'This might be illegal.'],
-
-   ['title' => 'US news 4',
-   'published' => '13.09.2025',
-   'author' => 'Morgan',
-   'body' => 'Say no to this.'],
-
-]
-?>
-
-
 <?php include __DIR__ . '/partials/header.php'; ?>
 
-    <main class="container">
+<main class="container">
 
 
-      <div class="row g-5">
-        <div class="col-md-8">
-          <?php include __DIR__ . '/partials/posts.php'; ?>
-        </div>
-        <div class="col-md-4">
-          <?php include __DIR__ . '/partials/sidebar.php'; ?>
-        </div>
-      </div>
-    </main>
+  <div class="row g-5">
+    <div class="col-md-8">
+      <?php include __DIR__ . '/partials/posts.php'; ?>
+    </div>
+    <div class="col-md-4">
+      <?php include __DIR__ . '/partials/sidebar.php'; ?>
+    </div>
+  </div>
+</main>
 <?php include __DIR__ . '/partials/footer.php'; ?>

4576456c167cbbe41bf2958a55f1dcc05126dbb8
kellyh06 <kelly.hurt@tptlive.ee>
2025-09-30 11:46:34 +0300

    Add db

---
+++ src/Controllers/PostsController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controllers;
+
+use App\DB;
+class PostsController
+{
+    public function index()
+    {
+        $db = new DB();
+        $posts = $db->all('posts');
+        dump($posts);
+        $users = $db->all('users');
+        dump($users);
+    }
+}

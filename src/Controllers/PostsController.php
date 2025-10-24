e9d654567d542e1c1aab28cac4a59b2c03565d8c
kellyh06 <kelly.hurt@tptlive.ee>
2025-10-24 18:41:23 +0300

    File upload

--- src/Controllers/PostsController.php
+++ src/Controllers/PostsController.php
@@ -8,6 +8,14 @@ use App\Models\User;
 
 class PostsController
 {
+    public function __construct()
+    {
+        if (!auth()) {
+            redirect('/login');
+            die();
+        }
+    }
+
     public function index()
     {
         $posts = Post::all();
@@ -21,7 +29,16 @@ class PostsController
 
     public function store()
     {
-        dump($_POST);
+        dd($_FILES, $_POST);
+        $from = $_FILES['image']['tmp_name'];
+        $ext = pathinfo($_FILES['image']['name'], PATHINFO_EXTENSION);
+        do {
+        $name = md5($_FILES['image']['name'] . microtime() . rand(PHP_INT_MIN, PHP_INT_MAX)) . '.' . $ext;
+        } while (file_exists(__DIR__ . '/../../public/uploads/' . $name));
+        $to = __DIR__ . '/../../public/uploads/' . $name;
+        move_uploaded_file($from, $to);
+
+        dd($_FILES, $_POST);
         $post = new Post();
         $post->title = $_POST['title'];
         $post->body = $_POST['body'];
@@ -30,6 +47,7 @@ class PostsController
     }
 
     public function view()
+
     {
         $post = Post::find($_GET['id']);
         view('posts/view', compact('post'));
@@ -57,5 +75,3 @@ class PostsController
         redirect('/posts');
     }
 }
-
-
--- views/posts/create.php
+++ views/posts/create.php
@@ -1,7 +1,7 @@
 <?php include __DIR__ . '/../partials/header.php'; ?>
 
 <main class="container">
-    <form action="/posts" method="POST">
+    <form action="/posts" method="POST" enctype="multipart/form-data">
     <div class="mb-3">
         <label for="title" class="form-label">Title</label>
         <input name="title" type="text" class="form-control" id="title" placeholder="Title">
@@ -9,6 +9,10 @@
     <div class="mb-3">
         <label for="body" class="form-label">Content</label>
         <textarea name="body" class="form-control" id="body" rows="12"></textarea>
+    </div>
+    <div class="mb-3">
+        <label for="image" class="form-label">Image</label>
+        <input name="image[]" type="file" class="form-control" id="image" accept="image/*" multiple>
     </div>
     <button class="btn btn-primary">Create</button>
 </form>
